f24aa54dd066ea8129940a9c7f7b58e6a4516880
Manish Rawat <rawatmani@gmail.com>
2025-03-17 11:38:19 -0400

    Complete grocery Project

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -7,47 +7,64 @@ import { GroceryService } from '../grocery.service';
 
 @Component({
   selector: 'app-grocery-list',
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './grocery-list.component.html',
   styleUrl: './grocery-list.component.css'
 })
 export class GroceryListComponent {
+
   newProduct: string = '';
   errorMessage: string = '';
-  groceryList: string[] = ['Bananas', 'Peanut Butter', 'Wheat Bread', 'Greek yogurt'];
-  //groceryList: any[] = [];
-  
-  constructor(private groceryService: GroceryService) {}
+  //groceryList: string[] = ['Bananas', 'Peanut Butter', 'Wheat Bread', 'Greek yogurt'];
+  groceryList: any[] = [];
 
+  constructor(private groceryService: GroceryService) { }
 
- /* ngOnInit() {
+  ngOnInit() {
+    this.loadGroceries();
+  }
+
+  loadGroceries() {
     this.groceryService.getGroceries().subscribe({
       next: (data) => {
-        this.groceryList = data;
+
+        for (let i = 0; i < data.length; i++) {
+          this.groceryList.push(data[i].itemName);
+        }
       },
       error: (error) => {
         console.error('Error fetching grocery list:', error);
       }
     });
-  }*/
+  }
 
   addProduct() {
+
+    // trim()==Prevents users from adding empty or whitespace-only product names
     if (!this.newProduct.trim()) {
       this.errorMessage = 'Empty or duplicate product not allowed';
       return;
     }
-
     if (this.groceryList.includes(this.newProduct.trim())) {
       this.errorMessage = 'Duplicate product not allowed';
       return;
     }
-console.log(this.newProduct);
+
+    if (this.groceryList.some(item => item.toLowerCase() === this.newProduct.trim().toLowerCase())) {
+      // The new product exists in the list in any case format
+      this.errorMessage = 'Duplicate product not allowed';
+      return;
+    }
     this.groceryList.push(this.newProduct.trim());
+
     this.newProduct = '';
     this.errorMessage = '';
   }
+  //splice(index, 1) removes one element from the array, starting at index.
+  //splice() modifies the original array directly.
 
   removeProduct(index: number) {
+
     this.groceryList.splice(index, 1);
   }
 }
--- src/app/grocery.service.ts
+++ src/app/grocery.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroceryService {
-  private apiUrl = 'https://dummyjson.com/products';
+  private apiUrl = 'http://localhost:3000/groceries';  // API endpoint
+
 
   constructor(private http: HttpClient) { }
 
   getGroceries(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+   
+        return this.http.get<any>(this.apiUrl);
+        
   }
 }
